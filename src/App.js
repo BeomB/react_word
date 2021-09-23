29153467af3340ffee01ab5e360c5f6734b03cd3
beombeom1119 <qjarl1119@naver.com>
2021-09-23 16:03:50 +0900

    Event Handle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import Css from './component/Css'
 import Hello from './component/Hello'
-import Test from './component/Test'
 import './App.css'
 
 const App = () => {
   
   return (
     <div className="App">
-    <Test/>
-    <Hello></Hello>
-    <Css></Css>
+    <Hello></Hello>  
     </div>
   )
 }
